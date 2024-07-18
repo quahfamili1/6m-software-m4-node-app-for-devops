8243d599b02992f589ae551e288c90701fc6ccd3
Alan Quah <quahfamili@live.com.sg>
2024-07-18 21:36:26 +0800

    update index.js

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ app.get('/', print);
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to port ${process.env.PORT}`)
 })
+
+app.get('/home', (req, res) => {
+    res.send('This is the home page')
+})
+
+app.post('/user', (req, res) => {
+    const {name, email, password} = req.body;
+    res.send(`Name: ${name}, Email: ${email}`)
+})
